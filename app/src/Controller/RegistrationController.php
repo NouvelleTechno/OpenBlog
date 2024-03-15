b6e4588c46824c2dce2ce23fd8d5940bdc529a8f
Nouvelle-Techno.fr <contact@nouvelle-techno.fr>
2024-03-15 07:55:46 +0100

    6 - Envoi du mail de confirmation du compte des utilisateurs

--- app/src/Controller/RegistrationController.php
+++ app/src/Controller/RegistrationController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Users;
 use App\Form\RegistrationFormType;
+use App\Repository\UsersRepository;
 use App\Security\UsersAuthenticator;
+use App\Service\JWTService;
+use App\Service\SendEmailService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,7 +19,7 @@ use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UsersAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UsersAuthenticator $authenticator, EntityManagerInterface $entityManager, JWTService $jwt, SendEmailService $mail): Response
     {
         $user = new Users();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -35,6 +38,32 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
+            // Générer le token
+            // Header
+            $header = [
+                'typ' => 'JWT',
+                'alg' => 'HS256'
+            ];
+
+            // Payload
+            $payload = [
+                'user_id' => $user->getId()
+            ];
+
+            // On génère le token
+            $token = $jwt->generate($header, $payload, $this->getParameter('app.jwtsecret'));
+
+            // Envoyer l'e-mail
+            $mail->send(
+                'no-reply@openblog.test',
+                $user->getEmail(),
+                'Activation de votre compte sur le site OpenBlog',
+                'register',
+                compact('user', 'token') // ['user' => $user, 'token'=>$token]
+            );
+
+            $this->addFlash('success', 'Utilisateur inscrit, veuillez cliquer sur le lien reçu pour confirmer votre adresse e-mail');
+
             return $userAuthenticator->authenticateUser(
                 $user,
                 $authenticator,
@@ -45,5 +74,30 @@ class RegistrationController extends AbstractController
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
+    }
+
+    #[Route('/verif/{token}', name: 'verify_user')]
+    public function verifUser($token, JWTService $jwt, UsersRepository $usersRepository, EntityManagerInterface $em): Response
+    {
+        // On vérifie si le token est valide (cohérent, pas expiré et signature correcte)
+        if($jwt->isValid($token) && !$jwt->isExpired($token) && $jwt->check($token, $this->getParameter('app.jwtsecret'))){
+            // Le token est valide
+            // On récupère les données (payload)
+            $payload = $jwt->getPayload($token);
+            
+            // On récupère le user
+            $user = $usersRepository->find($payload['user_id']);
+
+            // On vérifie qu'on a bien un user et qu'il n'est pas déjà activé
+            if($user && !$user->isVerified()){
+                $user->setIsVerified(true);
+                $em->flush();
+
+                $this->addFlash('success', 'Utilisateur activé');
+                return $this->redirectToRoute('app_main');
+            }
+        }
+        $this->addFlash('danger', 'Le token est invalide ou a expiré');
+        return $this->redirectToRoute('app_login');
     }
 }
---
+++ app/src/Service/SendEmailService.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Service;
+
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+
+class SendEmailService
+{
+    public function __construct(private MailerInterface $mailer)
+    {}
+
+    public function send(
+        string $from,
+        string $to,
+        string $subject,
+        string $template,
+        array $context
+    ): void
+    {
+        // On crée le mail
+        $email = (new TemplatedEmail())
+            ->from($from)
+            ->to($to)
+            ->subject($subject)
+            ->htmlTemplate("emails/$template.html.twig")
+            ->context($context);
+
+        // On envoie le mail
+        $this->mailer->send($email);
+    }
+}
