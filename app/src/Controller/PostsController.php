64dd22c768585d5effec3e868a585f1854a9b9cf
NouvelleTechno <contact@nouvelle-techno.fr>
2025-01-05 13:53:10 +0100

    19 - Liste des articles avec pagination

--- app/src/Controller/PostsController.php
+++ app/src/Controller/PostsController.php
@@ -4,12 +4,24 @@ namespace App\Controller;
 
 use App\Repository\PostsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route(path:'/articles', name:'app_posts_')]
 class PostsController extends AbstractController
 {
+    #[Route(path:'', name:'index')]
+    public function index(PostsRepository $postsRepository, Request $request): Response
+    {
+        // On récupère le numéro de page
+        $page = $request->query->get('page',1);
+
+        $data = $postsRepository->getAllPaginated($page, 2);
+        
+        return $this->render('posts/index.html.twig', compact('data'));
+    }
+
     #[Route(path:'/details/{slug}', name:'details')]
     public function details($slug, PostsRepository $postsRepository): Response
     {
--- app/src/Repository/PostsRepository.php
+++ app/src/Repository/PostsRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Posts;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -19,6 +20,23 @@ class PostsRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Posts::class);
+    }
+
+    public function getAllPaginated(int $page = 1, int $limit = 10): array
+    {
+        $offset = ($page - 1) * $limit;
+        $query = $this->createQueryBuilder('p')
+            ->orderBy('p.id', 'DESC')
+            ->setFirstResult($offset)
+            ->setMaxResults($limit);
+            
+        $paginator = new Paginator($query);
+        $data = $paginator->getQuery()->getResult();
+
+        $result['posts'] = $data;
+        $result['pages'] = ceil($paginator->count() / $limit);
+        $result['current'] = $page;
+        return $result;
     }
 
 //    /**
